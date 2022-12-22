bea80db5de6d8115cdcb1206663dc0c1959ed11b
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2022-12-22 18:07:14 -0300

    22/12/22

---
+++ programa.py
@@ -0,0 +1,14 @@
+mensagem = 'oi, python'
+numero = 5
+pi = 3.14
+
+print(mensagem)
+print(numero)
+print(pi)
+
+
+
+
+
+
+
---
+++ programa2.py
@@ -0,0 +1,16 @@
+#numero = input('Digite um número:\n')
+#print('O número digitado foi' + numero)
+
+#nome = input('Digite seu nome: ')
+#idade = input('Digite sua idade: ')
+#print('Seu nome é {} e sua idade é {}'.format(nome, idade))
+#print('Seu nome é ' + nome + ' e sua idade é ' + idade)
+
+x = 245.2346
+print('{:.2f}'.format(x))
+
+x = [1, 2, 3]
+y = [1, 2, 3]
+print(x == y) # verifica se o conteudo é igual
+print(x is y) # verifica se x e y são o msm objeto
+
